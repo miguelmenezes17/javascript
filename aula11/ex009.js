931dc1d4377349bc561a66b6418560f37b27dc0a
miguelmenezes17 <maniciomenezes@gmail.com>
2024-07-05 12:12:32 -0300

    Aula 11 - Curs de Javascript - cursoemvideo
    Aula 11 - Curs de Javascript - cursoemvideo - CONDIÇÕES (parte  1)

---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel} quilometros por hora!`)
+if (vel > 60) { //CONDIÇÃO SIMPLES, POIS NÃO POSSUI "SE NÃO:"
+    console.log(`Voce ultrapassou a velocidade permitida! MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
