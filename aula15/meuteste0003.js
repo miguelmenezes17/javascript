960645b5400255a49ec11be0300e1e90111a480f
miguelmenezes17 <maniciomenezes@gmail.com>
2024-07-10 18:12:53 -0300

    Criei os arquivos da aula15 e aula16
    Criei os arquivos da aula15 e aula16

---
+++ aula15/meuteste0003.js
@@ -0,0 +1,8 @@
+var sequencia = [1, 5, 6, 5, 6, 4, 6, 8, 3, 1, 0]
+console.log(sequencia)
+console.log(`Nossa sequencia é a seguinte ${sequencia}`)
+console.log(`Podemos organiza-la, se quisermos:`)
+sequencia.sort()
+console.log(`Nosso vetor organiza é este: ${sequencia}`)
+sequencia.push(9)
+console.log(sequencia)
